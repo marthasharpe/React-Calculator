a7021f964633b593c96453117b77fe926f799f82
Martha Sharpe <marthasharpe2020@gmail.com>
2019-12-27 10:53:54 -0600

    Add signature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Calculator from './Calculator';
+import Signature from './Signature/Signature';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Signature />
     </div>
   );
 }
